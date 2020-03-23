4b170b054fa812e523839c4320bcabd7941424dd
224304 Guzek Pawel <224304@edu.p.lodz.pl>
2020-03-23 12:27:22 +0100

    fixed implementation of backtracking algorithm using shuffled list insted of Random; added/improved JavaDocs

--- src/main/java/pl/prokom/sudoku/BacktrackingSudokuSolver.java
+++ src/main/java/pl/prokom/sudoku/BacktrackingSudokuSolver.java
@@ -1,31 +1,27 @@
 package pl.prokom.sudoku;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+
+/**
+ * Implementation of Backtracking algorithm to solve sudoku.
+ */
 public class BacktrackingSudokuSolver implements SudokuSolver<SudokuBoard> {
 
     /**
-     * Generates/Fills sudoku board with numbers.
+     * Method to solve sudoku board.
      *
-     * @param sudoku, which is SudokuBoard type
+     * @param sudoku SudokuBoard object
      */
     public final void solve(final SudokuBoard sudoku) {
-
         int squareSize = sudoku.getSquareSize();
-        Random random = new Random();
         int[][] helpBoard = new int[squareSize][squareSize];
-
-        List<Integer> orderedList =
+        List<Integer> randomValues =
                 IntStream.range(1, squareSize + 1).boxed().collect(Collectors.toList());
-        Collections.shuffle(orderedList);
-
-        Integer[] randomValues = new Integer[squareSize];
-        orderedList.toArray(randomValues);
+        Collections.shuffle(randomValues);
 
         for (int row = 0; row < squareSize; row++) {
             for (int column = 0; column < squareSize; column++) {
@@ -35,24 +31,25 @@ public class BacktrackingSudokuSolver implements SudokuSolver<SudokuBoard> {
                 boolean isCorrect = false;
 
                 if (helpBoard[row][column] == 0) {
-                    helpBoard[row][column] = random.nextInt(squareSize) + 1;
+                    helpBoard[row][column] = 1;
                     int rand = helpBoard[row][column];
 
                     do {
-                        if (isAllowed(row, column, rand, sudoku)) {
-                            sudoku.setBoardCell(row, column, rand);
+                        if (isAllowed(row, column, randomValues.get(rand - 1), sudoku)) {
+                            sudoku.setBoardCell(row, column, randomValues.get(rand - 1));
                             isCorrect = true;
                             break;
                         }
                         rand = rand % squareSize + 1;
                     } while (rand != helpBoard[row][column]);
                 } else {
-                    int rand = sudoku.getBoardCell(row, column) % squareSize + 1;
+                    int rand = randomValues.indexOf(sudoku.getBoardCell(row, column)) + 1;
+                    rand = rand % squareSize + 1;
                     sudoku.setBoardCell(row, column, 0);
 
                     while (rand != helpBoard[row][column]) {
-                        if (isAllowed(row, column, rand, sudoku)) {
-                            sudoku.setBoardCell(row, column, rand);
+                        if (isAllowed(row, column, randomValues.get(rand - 1), sudoku)) {
+                            sudoku.setBoardCell(row, column, randomValues.get(rand - 1));
                             isCorrect = true;
                             break;
                         }
@@ -79,9 +76,11 @@ public class BacktrackingSudokuSolver implements SudokuSolver<SudokuBoard> {
      * @param row    int row to search for duplicates
      * @param column int column to search for duplicates
      * @param num    int number you want to put in board
-     * @return boolean
+     * @param sudoku SudokuBoard object
+     * @return whether is possible to put number in board
      */
-    public boolean isAllowed(final int row, final int column, final int num, final SudokuBoard sudoku) {
+    public boolean isAllowed(final int row, final int column, final int num,
+                             final SudokuBoard sudoku) {
         return isAllowedInRow(row, num, sudoku)
                 && isAllowedInColumn(column, num, sudoku)
                 && isAllowedInMiniSquare(row, column, num, sudoku);
@@ -130,7 +129,8 @@ public class BacktrackingSudokuSolver implements SudokuSolver<SudokuBoard> {
      * @param sudoku SudokuBoard object
      * @return whether is possible to put number in miniSquare
      */
-    private boolean isAllowedInMiniSquare(final int row, final int column, final int num, final SudokuBoard sudoku) {
+    private boolean isAllowedInMiniSquare(final int row, final int column, final int num,
+                                          final SudokuBoard sudoku) {
         int miniRow = row - row % sudoku.getMiniSquareSize();
         int miniColumn = column - column % sudoku.getMiniSquareSize();
 
