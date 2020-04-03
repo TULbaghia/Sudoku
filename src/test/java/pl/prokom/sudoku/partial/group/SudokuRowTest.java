d18cd0799b32e555efcb7604adb88186b77e3b04
224369 Maksajda Michał <224369@edu.p.lodz.pl>
2020-04-03 11:17:04 +0200

    SudokuRow test cases

--- src/test/java/pl/prokom/sudoku/partial/group/SudokuRowTest.java
+++ src/test/java/pl/prokom/sudoku/partial/group/SudokuRowTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pl.prokom.sudoku.partial.field.SudokuField;
 
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SudokuRowTest {
@@ -12,14 +15,32 @@ class SudokuRowTest {
 
     @BeforeEach
     void setUp() {
-        sudokuFields = new SudokuField[]{new SudokuField(1), new SudokuField(2)};
-        sudokuRow = new SudokuRow(sudokuFields.clone());
+        AtomicInteger index = new AtomicInteger(1);
+        sudokuFields = new SudokuField[9];
+        sudokuFields = Arrays.stream(sudokuFields)
+                .map(x -> new SudokuField(index.getAndIncrement()))
+                .toArray(SudokuField[]::new);
+
+        sudokuRow = new SudokuRow(sudokuFields);
     }
 
+
+    /**
+     * Case description:
+     * - getRow return the same object that was pushed in SudokuRow
+     */
     @Test
     void getRowTestCase() {
+        assertArrayEquals(sudokuFields, sudokuRow.getRow());
+        assertSame(sudokuFields, sudokuRow.getRow());
+        sudokuFields[0].resetValue();
+
         for (int i = 0; i < sudokuFields.length; i++) {
-            assertEquals(sudokuFields[i], sudokuRow.getRow()[i]);
+            assertSame(sudokuFields[i], sudokuRow.getRow()[i]);
         }
+
+        sudokuFields[1].resetValue();
+        assertTrue(Arrays.deepEquals(sudokuFields, sudokuRow.getRow()));
     }
+
 }
