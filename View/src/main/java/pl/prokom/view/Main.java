cb078aed7ef11f3a79730dfb065ea5c21d3d9762
224304 Paweł Guzek <224304@edu.p.lodz.pl>
2020-04-25 00:27:08 +0200

    added maven-{jar,assemly}-plugin to build executable View jar file

---
+++ View/src/main/java/pl/prokom/view/Main.java
@@ -0,0 +1,17 @@
+package pl.prokom.view;
+
+import pl.prokom.view.menu.SudokuBoardWindow;
+
+/**
+ * Jar bundler needs Main class that does not extends/implements any other class.
+ * This class is just a wrapper for jar-builder
+ */
+public class Main {
+    /**
+     * Default class of program that handles further execution.
+     * @param args Parameters to customize settings
+     */
+    public static void main(String[] args) {
+        SudokuBoardWindow.main(args);
+    }
+}
